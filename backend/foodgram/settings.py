f5093a12eaa9281949a6e596faf28bccec360655
Vitaliy Pavlov <v-2841@yandex.ru>
2023-08-02 21:55:13 +0400

    v0.2

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -64,16 +64,16 @@ WSGI_APPLICATION = 'foodgram.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('POSTGRES_DB', 'django'),
-        'USER': os.getenv('POSTGRES_USER', 'django_user'),
+        'NAME': os.getenv('POSTGRES_DB', 'foodgram'),
+        'USER': os.getenv('POSTGRES_USER', 'foodgram_user'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'mysecretpassword'),
         'HOST': os.getenv('DB_HOST', ''),
         'PORT': os.getenv('DB_PORT', 5432)
     },
     'TEST': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'django',
-        'USER': 'django_user',
+        'NAME': 'test',
+        'USER': 'foodgram_user',
         'PASSWORD': 'mysecretpassword',
         'HOST': '',
         'PORT': '',
